fa3a736d6239b3cc3c9be61099a472166a5ae3f4
FilyanovichNP <nata.filyanovich@gmail.com>
2023-12-20 22:58:12 +0300

    Lesson 13

--- trainee/src/test/java/org/example/BlockOnlineTest.java
+++ trainee/src/test/java/org/example/BlockOnlineTest.java
@@ -13,26 +13,18 @@ import java.util.concurrent.TimeUnit;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BlockOnlineTest {
-        static WebDriver driver;
+    static WebDriver driver;
 
     @BeforeAll
     public static void setup() {
-        System.setProperty("webdriver.chrome.driver",new File("src/test/resources/chromedriver.exe").getAbsolutePath());
-        WebDriver driver= new ChromeDriver();
+        System.setProperty("webdriver.chrome.driver", new File("src/test/resources/chromedriver.exe").getAbsolutePath());
+        WebDriver driver = new ChromeDriver();
         driver.get("https://www.mts.by/");
         driver.manage().window().maximize();
         //задержка на выполнение теста = 10 сек.
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
     }
-//    @Test
-//    public void AllTests() {
-//        System.setProperty("webdriver.chrome.driver", new File("src/test/resources/chromedriver.exe").getAbsolutePath());
-//        driver = new ChromeDriver();
-//        driver.get("https://www.mts.by/");
-//        driver.manage().window().maximize();
-//
-//    }
 
     @Test
     public void nameBlockTest() {
@@ -41,15 +33,12 @@ public class BlockOnlineTest {
         String nameBlock = driver.findElement(By.xpath("//div[@class = 'pay__wrapper']/h2")).getText();
         assertEquals("Онлайн пополнение\n" +
                 "без комиссии", nameBlock);
-
     }
 
     @Test
-    public void logoTest(){
+    public void logoTest() {
         driver = new ChromeDriver();
         driver.get("https://www.mts.by/");
-
-
     }
 
     @AfterAll

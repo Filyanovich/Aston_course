ca58628f0533134082cc739a1097e5252fb0ed03
FilyanovichNP <nata.filyanovich@gmail.com>
2023-12-26 21:49:30 +0300

    update project

---
+++ src/main/java/lessons/Main.java
@@ -0,0 +1,14 @@
+package lessons;
+
+import java.util.stream.LongStream;
+
+public class Main {
+    public static long factorial(int number){
+        return LongStream.rangeClosed(1, number)
+                .reduce(1, (long x, long y) -> x * y);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(factorial(5));
+    }
+}

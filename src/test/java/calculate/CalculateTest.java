0f20bbef585ce32d4307d2cb0df3814dc96148dc
FilyanovichNP <nata.filyanovich@gmail.com>
2024-01-05 23:20:28 +0300

    Lesson 18

--- src/test/java/calculate/CalculateTest.java
+++ src/test/java/calculate/CalculateTest.java
@@ -3,10 +3,12 @@ package calculate;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.AndroidMobileCapabilityType;
 import io.appium.java_client.remote.MobileCapabilityType;
+import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
@@ -15,13 +17,16 @@ import java.util.concurrent.TimeUnit;
 
 import static org.testng.AssertJUnit.assertEquals;
 
+@Listeners(io.qameta.allure.testng.AllureTestNg.class)
 public class CalculateTest {
 
     AndroidDriver driver = null;
 
+    @Step("Запуск приложения")
     @BeforeClass
     public void initialise() {
         DesiredCapabilities capabilities = new DesiredCapabilities();
+
         capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Galaxy A51");
         capabilities.setCapability("uiautomator2ServerInstallTimeout", 60000);
         capabilities.setCapability(AndroidMobileCapabilityType.APP_PACKAGE, "com.google.android.calculator");
@@ -44,25 +49,30 @@ public class CalculateTest {
     private By resultButton = By.id("com.google.android.calculator:id/eq");
     private By result = By.id("com.google.android.calculator:id/result_final");
 
+    @Step("Проверка операции сложения")
     @Test
     public void additionTest() {
         driver.findElement(By.id("com.google.android.calculator:id/digit_2")).click();
         driver.findElement(plus).click();
         driver.findElement(By.id("com.google.android.calculator:id/digit_3")).click();
         driver.findElement(resultButton).click();
+
         assertEquals(driver.findElement(result).getText(), "5");
 
     }
 
+    @Step("Проверка операции вычитания")
     @Test
     public void subtractionTest() {
         driver.findElement(By.id("com.google.android.calculator:id/digit_8")).click();
         driver.findElement(minus).click();
         driver.findElement(By.id("com.google.android.calculator:id/digit_1")).click();
         driver.findElement(resultButton).click();
+
         assertEquals(driver.findElement(result).getText(), "7");
     }
 
+    @Step("Проверка операции деления")
     @Test
     public void dividedTest() {
         driver.findElement(By.id("com.google.android.calculator:id/digit_1")).click();
@@ -73,6 +83,7 @@ public class CalculateTest {
         assertEquals(driver.findElement(result).getText(), "4");
     }
 
+    @Step("Проверка операции умножения")
     @Test
     public void multipliedTest() {
         driver.findElement(By.id("com.google.android.calculator:id/digit_2")).click();
@@ -82,6 +93,7 @@ public class CalculateTest {
         assertEquals(driver.findElement(result).getText(), "18");
     }
 
+    @Step("Закрыть сессию")
     @AfterClass
     public void quitDriver() {
         driver.quit();
